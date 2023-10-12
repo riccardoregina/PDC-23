c730eda09fb88c6e7d60c3eab078673f470f8498
riccardoregina <126416822+riccardoregina@users.noreply.github.com>
2023-10-12 20:19:27 +0200

    Update strategia2.c

--- SommaDiEnneNumeri/strategia2.c
+++ SommaDiEnneNumeri/strategia2.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include "mpi.h"
 
+int main()
+{
+    //Acquisizione dei dati
+
+    //Distribuzione dei dati
+
+    //Algoritmo
+    int i;
+    for (i = 0; i < log2(n_processi); i++) {
+        if (pid % pow(2,i) == 0) {
+            if (pid % pow(2,i+1) == 0) {
+                tag = pid;
+                //riceve
+                MPI_Recv
+            } else {
+                tag = abs(pow(2,i) - pid) //da controllare funzione abs
+                //manda
+                MPI_Send
+            }
+        }
+    }
+}
