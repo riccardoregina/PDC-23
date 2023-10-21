a2eda967a9fbac9bf88ec3ca191bbdfe3724c737
riccardoregina <126416822+riccardoregina@users.noreply.github.com>
2023-10-21 17:13:22 +0200

    Update Grafico.py

--- SommaDiEnneNumeri/Grafico.py
+++ SommaDiEnneNumeri/Grafico.py
@@ -1,22 +1,21 @@
 # Importing libraries 
 import matplotlib.pyplot as plt 
-import numpy as np
-  
-# Using Numpy to create an array X 
-X = np.arange(0, 10, 1) 
 
-# Plotting both the curves simultaneously 
-plt.plot([1,2,3,4], color='b', label='SpeedUp') 
-plt.plot([1,3,1,3], color='r', label='Efficienza')
-plt.plot([0,4,5,2], color='g', label='Overhead')
+# Creating x-axis values
+x_values = [1, 2, 4, 8]  
 
-# Naming the x-axis, y-axis and the whole graph 
+# Plotting both the curves simultaneously (random values at the moment)
+plt.plot(x_values, [1,2.32,3.43,4.67], color='b', label='SpeedUp') 
+plt.plot(x_values, [1,3,1,3], color='r', label='Efficienza')
+plt.plot(x_values, [20,4,5,2], color='g', label='Overhead')
+
+# Naming the x-axis, y-axis and the whole graph
 plt.xlabel("Numero di processori") 
 plt.ylabel("Dimensione del problema") 
 plt.title("SpeedUp, Efficienza e Overhead") 
   
 # Adding legend, which helps us recognize the curve according to it's color 
 plt.legend() 
-  
+
 # To load the display window 
 plt.show()
