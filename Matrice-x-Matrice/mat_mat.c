fafc1f8ef9172cee87ff7df2dae639cd9e4baa15
JhonnyPixel <83840431+JhonnyPixel@users.noreply.github.com>
2023-12-26 10:39:11 +0100

    Update mat_mat.c

--- Matrice-x-Matrice/mat_mat.c
+++ Matrice-x-Matrice/mat_mat.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mpi.h"
+#include <math.h>
 
 #define ERRORE_ALLOCAZIONE_MEMORIA 1
 
-int main(){
+float **readMatrix(char *s,int *n);
+void create_torus_communicator(int m,MPI_Comm *grid);
+void extrapolate_row_column_comm(MPI_Comm grid,MPI_Comm *r,MPI_Comm *c);
+int mod(int a,int m);
+float ** allocMatrix(int m, int n);
+void freeMatrix(float **M);
+float** initMatrix(float** A, int m, int n);
+float** addMatrix(float** A, float** B, int size);
+float stdScalarProduct(float** A, float** B, const int i, const int j, int size);
+float** matrixMultiplication(float** A, float** B, int size);
+float** createSubmatrix(float** A, int size, int offsetRow, int offsetCol);
+void gestisciErrore(int code);
+
+int main(int argc,char *argv[]){
     float **A;
     float **B;
     float **C;
@@ -12,25 +26,36 @@ int main(){
     int n;
     int pid,nproc;
 
+    MPI_Init(&argc, &argv);
+
     MPI_Status status;
 
-    MPI_Comm_rank(MPI_COMM_WORLD,&pid);
+    //MPI_Comm_rank(MPI_COMM_WORLD,&pid);
     MPI_Comm_size(MPI_COMM_WORLD,&nproc);
     
-    A=readMatrix("matrix1.txt",&n);
-    B=readMatrix("matrix2.txt",&n);
+    A=readMatrix("/homes/DMA/PDC/2024/TRTFNC03B/mat_mat/matrix1.txt",&n);
+    B=readMatrix("/homes/DMA/PDC/2024/TRTFNC03B/mat_mat/matrix2.txt",&n);
 
     int m=sqrt(nproc); //suppongo che nproc sia un quadrato perfetto la cui radice divide n 
 
     MPI_Comm grid;
-    create_torus_communicator(m,&grid);
+    int dim = 2;                      
+    int dims[2];
+    dims[0]=m;
+    dims[1]=m;       
+    int wrap[2] = { 1 , 1};         
+    int optimize = 1;                 
+
+    MPI_Cart_create(MPI_COMM_WORLD, dim, dims, wrap, optimize, &grid);
+    //create_torus_communicator(m,&grid);
     MPI_Comm rows,cols;
-    extrapolate_row_column_comm(grid,&rows,&cols);
+    //extrapolate_row_column_comm(grid,&rows,&cols);
 
     int i,j;
     int grid_rank;
 
     MPI_Comm_rank(grid,&grid_rank);
+    printf("rank in griglia--->%d\n",grid_rank);
 
     int coords[2];
     MPI_Cart_coords(grid, grid_rank, 2, coords);
@@ -42,15 +67,16 @@ int main(){
     //allochiamo A  B  e C secondo blockSize
 
     C=allocMatrix(n,n);
-    A_local_temp= allocMatrix(blockSize, blockSize);
-    A_local= allocMatrix(blockSize, blockSize);
-    B_local= allocMatrix(blockSize, blockSize);
-    C_local= allocMatrix(blockSize, blockSize);  //inizializzare a 0
-    C_local = initMatrix(blockSize, blockSize);
-    C_local_temp= allocMatrix(blockSize, blockSize);
+    float **A_local_temp= allocMatrix(blockSize, blockSize);
+    float **A_local= allocMatrix(blockSize, blockSize);
+    float **B_local= allocMatrix(blockSize, blockSize);
+    float **C_local= allocMatrix(blockSize, blockSize);  //inizializzare a 0
+    C_local = initMatrix(C_local,blockSize, blockSize);
+    float **C_local_temp= allocMatrix(blockSize, blockSize);
     
     
     if(coords[0]==0 && coords[1]==0){
+        
         
 
         for(i=0;i<m;i++){
@@ -61,7 +87,7 @@ int main(){
                     A_local = createSubmatrix(A, blockSize, 0, 0);
                 }
                 else{
-                    
+                    //crea SottoMatrice secondo l offset
                     //float** toSend = createSubmatrix(A, blockSize, offsetRow, offsetCol); non abbiamo bisogno di creare la sottomatrice da mandare
                     //...
                     MPI_Datatype blockMatrix;
@@ -73,7 +99,8 @@ int main(){
                     MPI_Type_create_subarray(2, sizes, subsizes, starts, MPI_ORDER_C, MPI_FLOAT, &blockMatrix);
                     MPI_Type_commit(&blockMatrix);
                     MPI_Send(&(A[0][0]), 1, blockMatrix, (i*m)+j, (i*m)+j+20, grid); //opzione A: mandiamo blockSize vettori. opzioneB:creare ogni volta datatype a seconda dell offset
-                }   
+                }
+                
             }
         }
     }
@@ -100,9 +127,9 @@ int main(){
         }
 
         if(coords[0]==mod(coords[1]-i,m)){ //se sono sulla diagonale
-            C_local_temp=matrixMultiplication(A_local,B_local);         //ho quello che mi serve in A_local
+            C_local_temp=matrixMultiplication(A_local,B_local,blockSize);         //ho quello che mi serve in A_local
         }else{
-            C_local_temp=matrixMultiplication(A_local_temp,B_local);    //altrimenti lo ho in A_local_temp
+            C_local_temp=matrixMultiplication(A_local_temp,B_local,blockSize);    //altrimenti lo ho in A_local_temp
         }
 
         C_local=addMatrix(C_local,C_local_temp,blockSize);
@@ -147,8 +174,8 @@ float **readMatrix(char *s,int *n){
 void create_torus_communicator(int m,MPI_Comm *grid){
 
     int dim = 2;                      //The number of Dimensions
-    int dims[dim] = { m , m };        //The size of each Dimension
-    int wrap[dim] = { 1 , 1};         //Wraparound?
+    int dims[2] = { m , m };        //The size of each Dimension
+    int wrap[2] = { 1 , 1};         //Wraparound?
     int optimize = 1;                 //Optimize?
 
     MPI_Cart_create(MPI_COMM_WORLD, dim, dims, wrap, optimize, grid);
@@ -168,7 +195,7 @@ void extrapolate_row_column_comm(MPI_Comm grid,MPI_Comm *r,MPI_Comm *c){
 
 }
 
-int mod(a,m){
+int mod(int a,int m){
     int ret;
     if(a<0){
         while(a<0){
@@ -199,7 +226,8 @@ float ** allocMatrix(int m, int n) {
         return NULL;
     }
 
-    for(int i=1; i<m; i++) {
+    int i;
+    for(i=1; i<m; i++) {
         M[i] = M[i-1] + n;
     }
 
