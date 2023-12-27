68db24b504dc56aa3509736e7ee076fe32820dc4
riccardoregina <riccardoregina04@gmail.com>
2023-12-27 16:44:58 +0100

    aggiunge generatore matrice

---
+++ Matrice-x-Matrice/genMat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MOD 3
+
+int main(int argc, char* argv[]) {
+    FILE* fp = fopen("PATH/matrix.txt", "w");
+    
+    int dim = atoi(argv[1]);
+    int dimMatrix = dim * dim;
+
+    srand(time(NULL));
+
+    fprintf(fp, "%d\n", dim);
+    
+    int i;
+    for (i = 0; i < dimMatrix; i++) {
+        fprintf(fp, "%f ", rand() % MOD);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
