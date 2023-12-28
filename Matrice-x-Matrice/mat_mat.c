b391b3a214d40557a0b8340682285abaca2e74bd
JhonnyPixel <83840431+JhonnyPixel@users.noreply.github.com>
2023-12-28 15:57:09 +0100

    versione definitiva

--- Matrice-x-Matrice/mat_mat.c
+++ Matrice-x-Matrice/mat_mat.c
@@ -4,10 +4,12 @@
 #include <math.h>
 
 #define ERRORE_ALLOCAZIONE_MEMORIA 1
+#define ERRORE_CREAZIONE_GRIGLIA 2
+#define ERRORE_APERTURA_FILE 3
+
 
 float **readMatrix(char *s,int *n);
-void create_torus_communicator(int m,MPI_Comm *grid);
-void extrapolate_row_column_comm(MPI_Comm grid,MPI_Comm *r,MPI_Comm *c);
+void create_grid_communicator(int m,MPI_Comm *grid);
 int mod(int a,int m);
 float ** allocMatrix(int m, int n);
 void freeMatrix(float **M);
@@ -16,6 +18,7 @@ float** addMatrix(float** A, float** B, int size);
 float stdScalarProduct(float** A, float** B, const int i, const int j, int size);
 float** matrixMultiplication(float** A, float** B, int size);
 float** createSubmatrix(float** A, int size, int offsetRow, int offsetCol);
+void printResultsOnFile(char *path,double t,int nproc,int m,int n);
 void gestisciErrore(int code);
 
 int main(int argc,char *argv[]){
@@ -39,29 +42,14 @@ int main(int argc,char *argv[]){
     int m=sqrt(nproc); //suppongo che nproc sia un quadrato perfetto la cui radice divide n 
 
     MPI_Comm grid;
-    int dim = 2;                      
-    int dims[2];
-    dims[0]=m;
-    dims[1]=m;       
-    int wrap[2];
-    wrap[0]=1;
-    wrap[1]=1;         
-    int optimize = 0;                 
-
-    if(MPI_SUCCESS==MPI_Cart_create(MPI_COMM_WORLD, dim, dims, wrap, optimize, &grid)){
-        //printf("tutto ok");
-        
-    }
-    //create_torus_communicator(m,&grid);
-    //MPI_Comm rows,cols;
-    //extrapolate_row_column_comm(grid,&rows,&cols);
-
+                 
+    create_grid_communicator( m, &grid);
+    
     int i,j;
     int grid_rank;
 
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Comm_rank( grid, &grid_rank);
-    //printf("rank in griglia--->%d\n",grid_rank);
 
     int coords[2];
     MPI_Cart_coords(grid, grid_rank, 2, coords);
@@ -79,11 +67,6 @@ int main(int argc,char *argv[]){
     float **C_local= allocMatrix(blockSize, blockSize);  //inizializzare a 0
     C_local = initMatrix(C_local,blockSize, blockSize);
     float **C_local_temp= allocMatrix(blockSize, blockSize);
-
-        //printf("Arrivati all esecuzione\n");
-        //printf("sono il processore %d --->(%d,%d)\n",grid_rank,coords[0],coords[1]);
-        //printf("m--->%d\n",m);
-        //printf("blockSize--->%d\n",blockSize);
         
 
     
@@ -102,9 +85,8 @@ int main(int argc,char *argv[]){
                     B_local = createSubmatrix(B, blockSize, 0, 0);
                 }
                 else{
-                    //crea SottoMatrice secondo l offset
-                    //float** toSend = createSubmatrix(A, blockSize, offsetRow, offsetCol); non abbiamo bisogno di creare la sottomatrice da mandare
-                    //...
+                    //crea Datatype subarray secondo l offset e dividi la matrice in  sottomatrici.
+
                     MPI_Datatype blockMatrix;
                     
                     int sizes[2]    = {n,n};  
@@ -113,35 +95,24 @@ int main(int argc,char *argv[]){
 
                     MPI_Type_create_subarray(2, sizes, subsizes, starts, MPI_ORDER_C, MPI_FLOAT, &blockMatrix);
                     MPI_Type_commit(&blockMatrix);
-                    MPI_Send(&(A[0][0]), 1, blockMatrix, (i*m)+j, (i*m)+j+20, grid); //opzione A: mandiamo blockSize vettori. opzioneB:creare ogni volta datatype a seconda dell offset
-                    MPI_Send(&(B[0][0]), 1, blockMatrix, (i*m)+j, (i*m)+j+900, grid); //opzione A: mandiamo blockSize vettori. opzioneB:creare ogni volta datatype a seconda dell offset
+                    MPI_Send(&(A[0][0]), 1, blockMatrix, (i*m)+j, (i*m)+j+20, grid); //dividamo e mandiamo la matrice A
+                    MPI_Send(&(B[0][0]), 1, blockMatrix, (i*m)+j, (i*m)+j+900, grid); //dividiamo e mandiamo la matrice B
                 }
                 
             }
         }
     }
     else{
-        MPI_Recv(&(A_local[0][0]), blockSize*blockSize, MPI_FLOAT, 0, grid_rank+20, grid, &status); //opzione A: riceviamo blockSize vettori. opzioneB: riceviamo il sottoblocco diretto
-        MPI_Recv(&(B_local[0][0]), blockSize*blockSize, MPI_FLOAT, 0, grid_rank+900, grid, &status);
+        MPI_Recv(&(A_local[0][0]), blockSize*blockSize, MPI_FLOAT, 0, grid_rank+20, grid, &status); //riceviamo la sottomatrice di A in A_local
+        MPI_Recv(&(B_local[0][0]), blockSize*blockSize, MPI_FLOAT, 0, grid_rank+900, grid, &status);//riceviamo la sottomatrice di B in B_local
     }
-
-    /*
-    if(coords[0]!=0 || coords[1]!=0){
-    printf("sottomatrice A_locale processore %d --->\n",grid_rank);
-    for(i=0;i<blockSize;i++){
-            for(j=0;j<blockSize;j++){
-                printf("%f ",A_local[i][j]);
-            }
-        printf("\n");
-        }
-    printf("\n");
-    }
-    */
     
     
 
     // Inizio dell'algoritmo di Fox
-    
+
+    double startTime=MPI_Wtime();
+
     for(i=0;i<m;i++){ //l algoritmo di FOX finisce dopo m passi (scorro le diagonali)
 
         //mandiamo il blocco di A_locale ai processori sulla stessa riga
@@ -150,14 +121,12 @@ int main(int argc,char *argv[]){
             for(j=0;j<m;j++){
                 int dest=coords[0]*m + j;
                 if(dest!=grid_rank){        
-                    //printf("sono il processore (%d,%d), invio a %d con tag %d\n",coords[0],coords[1],dest,dest+80*(i+1));                                                        //esegui le send sulla riga
-                    MPI_Send(&(A_local[0][0]), blockSize*blockSize, MPI_FLOAT, dest, dest+80*(i+1),grid);
+                    MPI_Send(&(A_local[0][0]), blockSize*blockSize, MPI_FLOAT, dest, dest+80*(i+1),grid); //manda A_local a tutti quelli della mia riga
                 }
             }
         }
         else{
             int sorgente= (coords[0]*m) + (mod(coords[0]+i,blockSize));
-            //printf("sono il processore (%d,%d), ricevo da %d con tag %d\n",coords[0],coords[1], sorgente,grid_rank+80*(i+1));
             MPI_Recv(&(A_local_temp[0][0]), blockSize*blockSize, MPI_FLOAT, sorgente, grid_rank+80*(i+1), grid, &status); //ricevi sulla tua riga 
         }
         
@@ -169,7 +138,7 @@ int main(int argc,char *argv[]){
             C_local_temp=matrixMultiplication(A_local_temp,B_local,blockSize);    //altrimenti lo ho in A_local_temp
         }
 
-        C_local=addMatrix(C_local,C_local_temp,blockSize);
+        C_local=addMatrix(C_local,C_local_temp,blockSize); //addizioniamo il risultato a quelli precendenti
         
 
         //mandiamo il blocco di B_locale ai processori sulla stessa colonna ma sulla riga precedente
@@ -179,20 +148,13 @@ int main(int argc,char *argv[]){
 
     }
 
+    double endTime=MPI_Wtime();
+
     //fine algoritmo
-    /*
-    printf("sottomatrice C_locale processore %d --->\n",grid_rank);
-    for(i=0;i<blockSize;i++){
-            for(j=0;j<blockSize;j++){
-                printf("%f ",C_local[i][j]);
-            }
-        printf("\n");
-    }
-    printf("\n");
-    */
+    
 
 
-
+    //ricostruzione del risultato nel processore [0][0]
 
     if(coords[0]==0 && coords[1]==0){
         MPI_Datatype blockMatrix;
@@ -225,22 +187,19 @@ int main(int argc,char *argv[]){
         MPI_Send(&(C_local[0][0]), blockSize*blockSize, MPI_FLOAT, 0, grid_rank+500, grid);
     }
 
+    //calcoliamo il tempo totale (il max dei tempi)
+    double time=endTime-startTime;
+    double timetot;
 
+    MPI_Reduce(&time,&timetot,1,MPI_DOUBLE,MPI_MAX,0,grid);
     
 
-
+    //scriviamo i risultati su file
     if(coords[0]==0&&coords[1]==0){
-        for(i=0;i<n;i++){
-            for(j=0;j<n;j++){
-                printf("%f ",C[i][j]);
-            }
-        printf("\n");
-        }
-        printf("\n");
+        printResultsOnFile("risultati.txt",timetot,nproc,m,n);
     }
 
 
-    printf("Arrivati all esecuzione\n");
 
 
     MPI_Finalize();
@@ -268,29 +227,24 @@ float **readMatrix(char *s,int *n){
     return M;
 }
 
-void create_torus_communicator(int m,MPI_Comm *grid){
+void create_grid_communicator(int m ,MPI_Comm *grid){
 
-    int dim = 2;                      //The number of Dimensions
-    int dims[2] = { m , m };        //The size of each Dimension
-    int wrap[2] = { 1 , 1};         //Wraparound?
-    int optimize = 1;                 //Optimize?
+    int dim = 2;                      
+    int dims[2];
+    dims[0]=m;
+    dims[1]=m;       
+    int wrap[2];
+    wrap[0]=0;
+    wrap[1]=0;         
+    int optimize = 0;                 
 
-    MPI_Cart_create(MPI_COMM_WORLD, dim, dims, wrap, optimize, grid);
-
-}
-
-void extrapolate_row_column_comm(MPI_Comm grid,MPI_Comm *r,MPI_Comm *c){
-
-  int direction[2] = { 0, 1 };
-  MPI_Cart_sub(grid, direction, r);
-
- 
-  direction[0]=1; direction[1]=0;
-
-  MPI_Cart_sub(grid, direction, c);
-
+    if(MPI_SUCCESS!=MPI_Cart_create(MPI_COMM_WORLD, dim, dims, wrap, optimize, grid)){
+        gestisciErrore(ERRORE_CREAZIONE_GRIGLIA);
+    }
 
 }
+
+
 
 int mod(int a,int m){
     int ret;
@@ -388,10 +342,32 @@ float** createSubmatrix(float** A, int size, int offsetRow, int offsetCol) {
     return R;
 }
 
+void printResultsOnFile(char *path,double t,int nproc,int m,int n){
+    FILE *fp;
+    fp=fopen(path,"a");
+
+    if(fp==NULL){
+        gestisciErrore(ERRORE_APERTURA_FILE);
+    }
+
+    fprintf(fp,"dimensione matrice--->(%dx%d) \n",n,n);
+    fprintf(fp,"numero di processori--->%d  matrice(%dx%d) \n",nproc,m,m);
+    fprintf(fp,"tempo di esecuzione--->%e \n\n",t);
+
+    
+    fclose(fp);
+}
+
 void gestisciErrore(int code){
     switch(code){
         case ERRORE_ALLOCAZIONE_MEMORIA:
-            printf("Errore nell allocazione della matrice in memoria");
+            printf("Errore nell allocazione della matrice in memoria\n");
+            exit(-1);
+        case ERRORE_CREAZIONE_GRIGLIA:
+            printf("Errore nella creazione del communicator griglia\n");
+            exit(-1);
+        case ERRORE_APERTURA_FILE:
+            printf("Errore nell apertura del file\n");
             exit(-1);
         default:
             exit(-1);
